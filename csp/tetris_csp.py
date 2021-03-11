21b94dca232e1d1798b5bf67c85fc0ac9319eb17
singhv42 <vishay.singh@mail.utoronto.ca>
2021-03-10 21:19:11 -0500

    added file

--- csp/tetris_csp.py
+++ csp/tetris_csp.py
@@ -3,32 +3,45 @@ from .tetromino_puzzle_constraint import TetrominoPuzzleConstraint
 from tetris import TetrominoUtil
 
 class TetrisCSP(CSP):
+    def __init__(self, grid):
+        super().__init__()
+        self.update_grid(grid)
+        self.add_constraint(TetrominoPuzzleConstraint(grid))
 
-  def __init__(self, grid):
-    super().__init__()
-    self.update_grid(grid)
-    self.add_constraint(TetrominoPuzzleConstraint(grid))
+    def update_grid(self, grid):
+        self._grid = grid
+        self._rows = len(self._grid)
+        self._cols = len(self._grid[0])
 
-  def update_grid(self, grid):
-    self._grid = grid
-    self._rows = len(self._grid)
-    self._cols = len(self._grid[0])
+    def _build_domain(self, var):
+        # Question 1, your build domain implementation goes here.
 
-  def _build_domain(self, var):
-    # Question 1, your build domain implementation goes here.
+        # This method simply returns a domain suitable for the given variable where the variable in this case
+        # is a TetrominoVariable. Remember the domain for a variable is all possible positions which it
+        # could take. Since we are dealing with tetromino pieces don't forget that they can rotate, thus
+        # creating more possible options for the variable can take.
 
-    # This method simply returns a domain suitable for the given variable where the variable in this case
-    # is a TetrominoVariable. Remember the domain for a variable is all possible positions which it
-    # could take. Since we are dealing with tetromino pieces don't forget that they can rotate, thus
-    # creating more possible options for the variable can take.
+        # Return a list of values (a domain) for said variable in the form of ((row, col), rotation) where
+        # (row, col) is a possible position on the grid.
 
-    # Return a list of values (a domain) for said variable in the form of ((row, col), rotation) where
-    # (row, col) is a possible position on the grid.
+        # Helpful Functions:
+        # TetrominoUtil.rotation_limit(tetromino) --> returns the maximum amount of rotations which the tetromino
+        #                                             can take before resulting in its original position.
+        # Tetromino.get_pruned_dimensions() --> Returns row_count, col_count of the tetromino pruned piece.
 
-    # Helpful Functions:
-    # TetrominoUtil.rotation_limit(tetromino) --> returns the maximum amount of rotations which the tetromino
-    #                                             can take before resulting in its original position.
-    # Tetromino.get_pruned_dimensions() --> Returns row_count, col_count of the tetromino pruned piece.
-    
-    raise NotImplementedError("Build Domain not defined for TetrisCSP") 
-        
+        #new_var = tetris_variable.TetrisVariable(var.Variable, TetrominoUtil.copy(var.Tetromino))
+        limit = 1
+        shape = var.__str__()
+        domain = []
+        tetromino = TetrominoUtil.copy(var)
+        if shape != 'o':
+            limit += TetrominoUtil.rotation_limit(var)
+        for i in range(limit):
+            rotation = i
+            row_count, col_count = tetromino.get_pruned_dimensions()
+            for r in range(self._rows - row_count + 1):
+                for c in range(self._cols - col_count + 1):
+                    domain.append(((r, c), rotation))
+            tetromino.rotate()
+        return domain
+
